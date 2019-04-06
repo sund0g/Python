b03901b568654cc3940d1a1c4fb41d28c14c99a0
Scott Estes <scott.estes@sizmek.com>
2019-04-06 10:18:08 -0700

    Optimizing for time/space complexity

--- rmwsp.py
+++ rmwsp.py
@@ -1,18 +1,28 @@
-import re  # Python Regular Expressions module
+# This is an example of removing all whitespace, (not just spaces) from a
+# string using the Regular Expressions module re and .sub method to
+# replace all instances of whitespace with no space.
+#
+# Reference: https://docs.python.org/3/library/re.html
+#
+# the returned string is enclosed in "" to verify that leading/trailing
+# whitespace is being removed.
+#
+# Time complexity: O(re*strlen) where re is the size of the regular expression,
+# and strlen is the length of the input string.
+#
+# Space complexity: n as the string is only in the buffer.
+
+import re
 import sys
 
 def main ():
     try:
-        input_string = sys.argv[1]
+        # Substitute all whitespace (\s+) with no space ('') in input_string
+        print("\nstring = \"{}\"\n".format(re.sub(r'\s+', '', sys.argv[1])))
 
     except:
-        print(f"\nplease enter a string")
-
-    else:
-        # Substitute all whitespace (\s+) with nothing ('') in input_string
-        string_with_whitespace_removed = re.sub(r'\s+', '', input_string)
-
-        print(f"\nstring = {string_with_whitespace_removed}")
+        print(f"\nplease enter a \"-delimited string\n")
 
 if __name__ == "__main__":
     main()
+
